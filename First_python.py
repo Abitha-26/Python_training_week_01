1790428b92fa6248332f37f382eeb42e497478bf
unknown <abithad61@gmail.com>
2025-01-17 18:02:05 +0530

    secong practice

---
+++ First_python.py
@@ -0,0 +1,54 @@
+age = 18
+if age >=18:
+    print ("Eligible for vote")
+else:
+    print("you are not eligible for vote")
+
+
+
+number = 9
+if number ==3:
+    print ("not a largest number")
+elif number== 6:
+    print("not a largest number")
+else:  
+     print("largest number")
+
+
+
+num= 47
+if num % 5==0 and num % 11==0:
+   print("divisible by 5 &11")
+else:
+    print("not divisible by 5 & 11")
+
+
+
+year = 1679
+if year % 4 == 0:
+   print("It is leap year")
+else:
+    print("Not a leap year")  
+
+
+
+grade =33
+if grade>=90:
+    print("A+ grade")
+elif grade >=75:
+    print("B grade")
+elif grade >=50:
+    print("C grade")
+elif grade <50:
+    print("Fail")
+else:
+    print("the grade")
+
+
+
+num=35
+if num %4==0 and num %2==0:
+   print("the num is even")
+else:
+   print("the number is valid")
+    
